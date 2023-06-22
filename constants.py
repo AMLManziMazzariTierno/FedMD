cf58fd3f04d5f50283181cb1bc9957e1dabcfa43
Mazzarimovic22 <mattiamazzari@gmail.com>
2023-06-22 21:42:02 +0200

    fix: n_classes

--- constants.py
+++ constants.py
@@ -45,7 +45,7 @@ MODELS_BALANCED = {
   "N_subset": 5000, 
   "private_classes": [0,2,20,63,71,82],
   "public_classes": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-  "N_rounds": 30,
+  "N_rounds": 20,
   "N_logits_matching_round": 1, 
   "N_private_training_round": 4,
   "private_training_batchsize" : 5, 
@@ -70,15 +70,15 @@ MODELS_UNBALANCED = {
                           "CNN_64_128_192", "CNN_128_192_256", "CNN_128_128_128",
                           "ResNet20"],
     "N_agents": 10,
-    "N_samples_per_class": 20, # not 3
+    "N_samples_per_class": 20,
     "N_subset": 5000, 
     "private_classes": [0,1,7,9,12,18], # Super classes
     "public_classes": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    "N_rounds": 30,
+    "N_rounds": 20,
     "N_logits_matching_round": 1, 
-    "N_private_training_round": 10, # not 4
-    "private_training_batchsize" : 10, # not 5
-    "logits_matching_batchsize": 128, # not 256
+    "N_private_training_round": 10,
+    "private_training_batchsize" : 10,
+    "logits_matching_batchsize": 128,
     "result_save_dir": "./result_CIFAR_imbalanced/"
 }
 
